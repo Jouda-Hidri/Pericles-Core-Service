fa40999f4b71a72ef36c7af0bbc1f7cf50c57774
Jouda Hidri <hidrijouda@gmail.com>
2018-02-24 22:45:07 +0100

    Add unit test for VoteController

--- src/test/java/pericles/coreservice/controller/VoteControllerTest.java
+++ src/test/java/pericles/coreservice/controller/VoteControllerTest.java
@@ -40,22 +40,34 @@ public class VoteControllerTest {
 		MockitoAnnotations.initMocks(this);
 	}
 
+	/**
+	 * Test that the GET request to "/vote/{voter}/for/{candidate}"
+	 * {@link pericles.coreservice.api.VoteController#vote(long, long)} returns the
+	 * correct status
+	 */
 	@Test
 	public void testVote() throws Exception {
-		// int voter = 1;
-		// int candidate = 1;
-		// this.mockMvc.perform(get("/vote/{voter}/for/{candidate}",voter,
-		// candidate).andExpect(status().isOk());
+		int voter = 1;
+		int candidate = 1;
+		// TODO add test case when voter or candidate doesn't exist in database
+		this.mockMvc.perform(get("/vote/{voter}/for/{candidate}", voter, candidate)).andExpect(status().isOk());
 	}
 
+	/**
+	 * Test that the GET request to "/result"
+	 * {@link pericles.coreservice.api.VoteController#getResult()} returns the
+	 * correct status
+	 */
 	@Test
 	public void testGetResult() throws Exception {
-//		List<Candidate> listCandidates = new ArrayList<Candidate>();
-//		listCandidates.add(new Candidate("Han", "Solo"));
-//		listCandidates.add(new Candidate("Darth", "Vader"));
-//		
-//		when(service.getListCandidates()).thenReturn(listCandidates);
-		
+		List<Candidate> listCandidates = new ArrayList<Candidate>();
+		listCandidates.add(new Candidate("Han", "Solo"));
+		listCandidates.add(new Candidate("Darth", "Vader"));
+
+		when(service.getListCandidates()).thenReturn(listCandidates);
+
+		// TODO test the return
+
 		this.mockMvc.perform(get("/result")).andExpect(status().isOk());
 	}
 
